f70f5a0f4d5e61603bb842c865e783aa795423fc
aaartyomsss <aaartyomsss@gmail.com>
2024-01-20 17:06:18 +0200

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -31,7 +31,7 @@ function createApp(database: Database) {
     }
   }
 
-  function calculateCost(age: number | undefined, type: string, date: Date | undefined, baseCost: number) {
+  function calculateCost(age: number | undefined, type: string, date: Date | undefined | Temporal.PlainDate, baseCost: number) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
